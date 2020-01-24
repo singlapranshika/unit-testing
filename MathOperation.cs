fdb1b8aeae1764a6bc6a89f46f7ea0b88aaaa257
singlapranshika <44138859+singlapranshika@users.noreply.github.com>
2020-01-24 11:29:04 +0530

    Add files via upload

---
+++ MathOperation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCalculator1
+{
+       public class MathOperation
+        {
+            public double Add(double number1, double number2)
+            {
+                return number1 + number2;
+            }
+            public double Subtract(double number1, double number2)
+            {
+                return number1 - number2;
+            }
+            public double Multiply(double number1, double number2)
+            {
+                return number1 * number2;
+            }
+            public double Divide(double number1, double number2)
+            {
+                return number1 / number2;
+            }
+            static void Main(string[] args)
+            {
+                MathOperation m = new MathOperation();
+                double number1, number2, result = 0;
+                number1 = Convert.ToDouble(Console.ReadLine());
+                number2 = Convert.ToDouble(Console.ReadLine());
+                String ch;
+                ch = Console.ReadLine();
+                switch (ch)
+                {
+                    case "+":
+                        result = m.Add(number1, number2);
+                        break;
+                    case "-":
+                        result = m.Subtract(number1, number2);
+                        break;
+                    case "*":
+                        result = m.Multiply(number1, number2);
+                        break;
+                    case "/":
+                        result = m.Divide(number1, number2);
+                        break;
+
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
---
+++ UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathCalculator1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+           MathOperation bm = new MathOperation();
+            double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+        [TestMethod]
+        public void Test_SubtractMethod()
+        {
+            MathOperation bm = new MathOperation();
+            double res = bm.Subtract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            MathOperation bm = new MathOperation();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(res, 100);
+        }
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            MathOperation bm = new MathOperation();
+            double res = bm.Divide(10, 10);
+            Assert.AreEqual(res, 1);
+        }
+    }
+}
